8cb07d4cd48146bd94db2b3d88259355b9e9dd99
karanspot <60371708+karanspot@users.noreply.github.com>
2025-01-22 09:30:56 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,13 +10,60 @@ resource "aws_instance" "example" {
 
   # Adding user data to run a script at launch
   user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install -y httpd
-              systemctl start httpd
-              systemctl enable httpd
-              echo "Hello, World from $(hostname -f)" > /var/www/html/index.html
-              EOF
+              #!/bin/bash  
+
+# GitHub credentials  
+
+github_user=<Your GitHub User>
+
+github_repo=<Your GitHub Repository>
+
+PAT=<Your GitHub Token>
+
+# Download jq for extracting the Token  
+
+yum install jq -y  
+
+# Create and move to the working directory  
+
+mkdir /actions-runner && cd /actions-runner  
+
+# Download the latest runner package
+
+curl -o actions-runner-linux-x64-2.304.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.304.0/actions-runner-linux-x64-2.304.0.tar.gz
+
+# Extract the installer  
+
+tar xzf ./actions-runner-linux-x64-2.304.0.tar.gz
+
+# Change the owner of the directory to ec2-user  
+
+chown ec2-user -R /actions-runner  
+
+# Get instance id to set it as a runner name  
+
+MetadataToken=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+
+instance_id=$(curl -H "X-aws-ec2-metadata-token: $MetadataToken" http://169.254.169.254/latest/meta-data/instance-id)
+
+echo "$instance_id"
+
+# Get the runner's token  
+
+token=$(curl -s -XPOST -H "authorization: token $PAT" https://api.github.com/repos/$github_user/$github_repo/actions/runners/registration-token | jq -r .token)  
+
+# Create the runner and start the configuration experience  
+
+sudo -u ec2-user ./config.sh --url https://github.com/${github_user}/${github_repo} --token $token --name "${instance_id}" --labels spot --unattended
+
+# Create the runner's service  
+
+./svc.sh install  
+
+# Start the service  
+
+./svc.sh start
+EOF
 
   tags = {
     Name = "ExampleInstance"
